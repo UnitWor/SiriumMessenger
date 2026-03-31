5697aa366eb4a22b27770a1cb4734f27bcc48ea6
KirillSharonov <154874212+Kirill-wq333@users.noreply.github.com>
2026-03-31 15:24:36 +0400

    indication = null

--- androidApp/src/main/java/com/messenger/androidapp/ui/presentation/navigation/bottomBar/SiriumBottomBar.kt
+++ androidApp/src/main/java/com/messenger/androidapp/ui/presentation/navigation/bottomBar/SiriumBottomBar.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -23,6 +24,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -175,7 +177,7 @@ fun SiriumBottomBar(
     Row(
         modifier = modifier
             .padding(bottom = 34.dp)
-            .liquid(liquidState = liquidState){
+            .liquid(liquidState = liquidState) {
                 frost = 5.dp
                 shape = RoundedCornerShape(32.dp)
                 refraction = 0.35f
@@ -225,7 +227,11 @@ fun BottomBarItem(
 
     Box(
         modifier = Modifier
-            .clickable(onClick = onClick)
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null,
+                onClick = onClick
+            )
     ) {
         Icon(
             imageVector = ImageVector.vectorResource(icon),
